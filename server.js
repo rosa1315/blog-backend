3b494c2a17a167c2e27dc50434255b8a6d1f9316
rosa1315 <rosayepes03@gmail.com>
2025-08-05 20:36:33 -0500

    link

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const authRoutes = require('./routes/authRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://blog-rosa-pink-gamma.vercel.app/', 
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/posts',  postRoutes);
